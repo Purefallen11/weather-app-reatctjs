6159769f896799f67f0036b7c8e55ae9e8fc7731
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-28 16:04:07 -0500

    made buttons, api renders perfectly, i am happy

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
 import WeatherCard from './WeatherCard'
+import {Button} from 'reactstrap'
 
 function App() {
 
   const [current, setCurrent] = useState([])
   const [location, setLocation] = useState([])
   const [condition, setCondition] = useState([])
+  const [city, setCity] = useState('las vegas')
+
+  
 
   useEffect(() => {
     axios
-    .get(`https://api.weatherapi.com/v1/current.json?key=127534eed2164d31aa1155956212806&q=Dallas&aqi=no
+    .get(`https://api.weatherapi.com/v1/current.json?key=127534eed2164d31aa1155956212806&q=${city}&aqi=no
 `)
       .then(res => {
         console.log(res)
@@ -22,11 +26,13 @@ function App() {
       .catch(err => {
       console.log("something went wrong", err)
     })
-  },[])
+  },[city])
 
   return (
     <div className="App">
-      <WeatherCard current={current} location={location} condition={condition}/>
+      <WeatherCard current={current} location={location} condition={condition} />
+      <Button color="primary" onClick={() => setCity('dallas')}>Dallas</Button>
+      <Button color="primary" onClick={()=> setCity('austin')}>Austin</Button>
     </div>
   );
 }
